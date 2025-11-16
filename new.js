88f87d7e2f7084469673433940eb4965412014ac
Anastasia Pichugina <a.pichugina@innopolis.university>
2025-11-16 22:29:19 +0300

    Initial commit

---
+++ new.js
@@ -0,0 +1,38 @@
+// {"id":122,
+//   "title":"Объявление 122: Недвижимость для продажи",
+//   "description":"Подробное описание товара 122. Это отличный товар, который подходит для различных целей. Качество проверено временем и имеет гарантию.",
+//   "price":70518,
+//   "category":"Недвижимость",
+//   "categoryId":1,
+//   "status":"approved",
+//   "priority":"normal",
+//   "createdAt":"2025-11-15T09:21:38.974Z",
+//   "updatedAt":"2025-11-15T11:17:57.369Z",
+//   "images":["https://placehold.co/300x200/cccccc/969696?text=Image+122-1","https://placehold.co/300x200/cccccc/969696?text=Image+122-2",
+//  "https://placehold.co/300x200/cccccc/969696?text=Image+122-3"],
+//  "seller":
+//     {"id":787,
+//       "name":"Продавец 53",
+//       "rating":"1.3",
+//       "totalAds":3,
+//       "registeredAt":"2025-08-02T09:21:38.974Z"
+//     },
+//   "characteristics":
+//     {"Состояние":"Удовлетворительное",
+//       "Гарантия":"Частичная",
+//       "Производитель":"Бренд T",
+//       "Модель":"Модель 440",
+//       "Цвет":"Зеленый"
+//     },
+//   "moderationHistory":
+//   [{
+//     "id":1,
+//     "moderatorId":5,
+//     "moderatorName":"Модератор 5",
+//     "action":"rejected",
+//     "reason":"Подозрение на мошенничество",
+//     "comment":"Объявление не соответствует правилам платформы",
+//     "timestamp":"2025-11-15T09:29:38.974Z"},
+//     {"id":2,"moderatorId":1,"moderatorName":"Алексей Петров","action":"approved","reason":null,"comment":"Объявление одобрено модератором","timestamp":"2025-11-15T10:18:04.419Z"},{"id":3,"moderatorId":1,"moderatorName":"Алексей Петров","action":"approved","reason":null,"comment":"Объявление одобрено модератором","timestamp":"2025-11-15T10:18:26.682Z"},{"id":4,"moderatorId":1,"moderatorName":"Алексей Петров","action":"approved","reason":null,"comment":"Объявление одобрено модератором","timestamp":"2025-11-15T11:17:57.369Z"}]}
+
+console.log(+"dsknd")
